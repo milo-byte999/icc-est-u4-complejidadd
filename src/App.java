5fd0705c68e6d32b61a145af20a4d7e1636190fa
Sebastian Arenillas - Gabriel Cuenca <sebasarenillas10@gmail.com>
2026-04-10 12:52:18 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int num=10;
+        int[] array = new int[] {1,2,3,4,5};
+        System.out.println(num);
+        System.out.println(array);
+        
     }
 }
