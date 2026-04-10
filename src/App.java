d9303340803202d26f49323e29d734ed356afe29
Sebastian Arenillas - Gabriel Cuenca <sebasarenillas10@gmail.com>
2026-04-10 12:54:09 -0500

    UPD1: App file

--- src/App.java
+++ src/App.java
@@ -6,6 +6,13 @@ public class App {
         int[] array = new int[] {1,2,3,4,5};
         System.out.println(num);
         System.out.println(array);
-        
+
+        for(int i=0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+        for (int i : array){
+            System.out.println(i);
+        }
     }
 }
